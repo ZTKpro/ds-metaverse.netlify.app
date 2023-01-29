8a070e258ddc9d5b9dc8670669cefc72d3a40352
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2023-01-29 18:25:30 +0100

    try models

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "aframe";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import sky from "./stars.jpg";
+import room from "./models/room.glb";
 
 function App() {
   const loader = new GLTFLoader();
+
+  loader.load(room, (d) => {
+    const entity = document.getElementById("room");
+    entity.object3D.add(d.scene);
+  });
   return (
     <a-scene>
-      <a-assets>
-        <img id="sky" src={sky} alt="sky" />
-      </a-assets>
-      <a-sky color="#FFFFFFF" material="src: sky" rotation="0 0 0"></a-sky>
+      <a-entity light="castShadow: true"></a-entity>
+      <a-entity id="room" gltf-model={room} position="0 0 0"></a-entity>
+      <a-light
+        id="dirlight"
+        intensity="1"
+        light="castShadow:true;type:directional"
+        position="0 1 0"
+      ></a-light>
     </a-scene>
   );
 }
